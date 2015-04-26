9968aad924fee0c80b895bc24e1452f5c525ee9e
Ilya Kreymer <ikreymer@gmail.com>
2015-04-26 09:02:25 -0700

    add invite sender

---
+++ invite.py
@@ -0,0 +1,17 @@
+from auth import init_manager_for_invite
+import sys
+
+def main():
+    m = init_manager_for_invite()
+    res = m.send_invite(sys.argv[1],
+                        email_template='templates/emailinvite.html',
+                        host='http://localhost:8088')
+    if res:
+        print('Success')
+    else:
+        print('Fail')
+
+if __name__ == "__main__":
+    main()
+
+
