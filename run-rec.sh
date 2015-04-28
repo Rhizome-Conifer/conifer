3c99fffad1984a3ac9083568f2bb8fb6ac25b585
Ilya Kreymer <ikreymer@gmail.com>
2015-04-27 20:49:31 -0700

    warcprox exec: increase timeout on rotate, decrease dedup timeout

--- run-rec.sh
+++ run-rec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-warcprox --redis-dedup-url redis://localhost/1 -z -d ./ -p 9002 --prefix wfa -m -i --rollover-idle-time 300
+warcprox --redis-dedup-url redis://localhost/1 -z -d ./ -p 9002 --prefix rec -m -i --rollover-idle-time 1200 --redis-dupe-timeout 10
 
 #warcprox --warc-per-url -z -p 9001 -d ./ --rollover-idle-time 60 -j ./dedup.db --read-buff-size 1000000 -n rec --certs-dir ./certs/ --base32 --cacert ./ca-cert.pem
