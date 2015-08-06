ce79e2948972b58b3620b97ca6c86eeb23baf23a
Ilya Kreymer <ikreymer@gmail.com>
2015-08-06 02:02:17 +0000

    embed frame: turn off scroll on inner frame docker rebuild: first rebuild, then restart

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # redeploy just webrecorder and nginx
-docker-compose stop webrecorder;
 docker-compose build webrecorder;
+docker-compose stop webrecorder;
 docker-compose up -d --no-deps webrecorder nginx
