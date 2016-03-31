139850d07bc61ae4b55c38700bcf0512d8f7127d
Masha Lifshin <mlifshin@gmail.com>
2016-03-31 13:02:17 -0700

    test api: add validations and li'l data store

--- webrecorder/test/api/v1.py
+++ webrecorder/test/api/v1.py
@@ -2,46 +2,79 @@ from bottle import get, post, request, response, run
 
 from random import randint
 import re
+import time
+
+# Li'l datastore
+USERNAME = "proust4eva"
+COLLECTION =  {"id": "proust-over-http", "title": "Proust over http",
+                "description": "What would Proust post?", "size_remaining": 143211060,
+                "recordings": [{"id": "a-la-recherche", "title": "À la recherche",
+                            "created_at": "2015010203000000", "updated_at": "2015010303000000",
+                            "size": 124288000},
+                            {"id": "du-temps-perdu", "title": "du temps perdu",
+                            "created_at": "2016010203000000", "updated_at": "2016010303000000",
+                            "size": 256788940}]}
+PAGES = { "a-la-recherche": [{"url": "http://twitter.com/proustfan36", "title": "Twitter - proustfan36", "timestamp": "2013140000000000"},
+                            {"url": "http://societyofmadelineeaters.com", "title": "The Society of Madeline Eaters", "timestamp": "2015020304000000"}],
+          "du-temps-perdu": [{"url": "http://involuntarymemory.guru", "title": "Involuntary Memory", "timestamp": "2016050607000000"}] }
 
 # GET /recordings
 @get('/api/v1/recordings')
 def recordings_index():
-    user = request.query.user
-    collection = request.query.collection
+    if not valid_user(request.query.u):
+        response.status = 404
+        return {"error_message": "Username not found"}
 
-    return { "recordings": [{"id": "a-la-recherche", "title": "À la recherche",
-                            "created_at": "2016010203000000", "updated_at": "2016010203000000",
-                            "size": 1000000},
-                            {"id": "du-temps-perdu", "title": "du temps perdu",
-                            "created_at": "2016010203000000", "updated_at": "2016010203000000",
-                            "size": 1000000}]}
+    if not valid_collection(request.query.c):
+        response.status = 404
+        return {"error_message": "Collection not found"}
+
+    return {"recordings" : COLLECTION['recordings']}
 
 # POST /recordings
 @post('/api/v1/recordings')
 def create_recording():
+    if not valid_user(request.query.u):
+        response.status = 404
+        return {"error_message": "Username not found"}
+
+    if not valid_collection(request.query.c):
+        response.status = 404
+        return {"error_message": "Collection not found"}
+
     title = request.forms.get('title')
     id = id_from_title(title)
 
-    if is_valid(id):
-        response.status = 200
-        return {"status": "success",
-                "recording": {"id": id, "title": title, "created_at": "2016010203000000",
-                              "modified_at": "2016010203000000", "size": 0}}
-    else:
+    recording = get_recording_by_id(id)
+    if recording:
         response.status = 400
         return  {"status": "AlreadyExists",
-                 "recording": {"id": id, "title": title,
-                 "created_at": "2016010203000000", "updated_at": "2016010203000000", "size": 100000}}
+                 "recording": recording}
+    else:
+        COLLECTION['recordings'].append({"id": id, "title": title, "created_at": int(round(time.time() * 1000)),
+                              "modified_at": int(round(time.time() * 1000)), "size": 0})
+        response.status = 200
+        return {"status": "success",
+                "recording": get_recording_by_id(id)}
 
 # GET /recordings/<id>
 @get('/api/v1/recordings/<id>')
 def get_recording(id):
-    user = request.query.user
-    collection = request.query.collection
+    if not valid_user(request.query.u):
+        response.status = 404
+        return {"error_message": "Username not found"}
 
-    title = title_from_id(id)
-    return {"id": id, "title": title, "created_at": "2016010203000000", "updated_at": "2016010203000000", "size": 87000}
+    if not valid_collection(request.query.c):
+        response.status = 404
+        return {"error_message": "Collection not found"}
 
+    recording = get_recording_by_id(id)
+    if recording:
+        response.status = 200
+        return recording
+    else:
+        response.status = 404
+        return {"error_message" : "Recording not found"}
 
 # @delete('/api/v1/recordings/<id>')
 # @get('/api/v1/recordings/<id>/download')
@@ -49,24 +82,36 @@ def get_recording(id):
 # GET /api/v1/recordings/<id>/pages
 @get('/api/v1/recordings/<id>/pages')
 def get_pages(id):
-    return {"pages": [{"url": "http://twitter.com/proustfan36", "title": "Twitter - proustfan36", "timestamp": "2013140000000000"},
-                      {"url": "http://societyofmadelineeaters.com", "title": "The Society of Madeline Eaters", "timestamp": "2016010203000000"}] }
+    if not valid_user(request.query.u):
+        response.status = 404
+        return {"error_message": "Username not found"}
+
+    if not valid_collection(request.query.c):
+        response.status = 404
+        return {"error_message": "Collection not found"}
+
+    if get_recording_by_id(id):
+        response.status = 200
+        return {"pages": PAGES[id]}
+    else:
+        response.status = 404
+        return {"error_message" : "Recording not found"}
 
 # @post('/api/v1/recordings/<id>/pages')
-
 # @get('/api/v1/collections')
 # @post('/api/v1/collections')
 # @get('/api/v1/collections/<id>')
 # @delete('/api/v1/collections/<id>')
 # @get('/api/v1/collections/<id>/download')
 
-# Utilities
-def is_valid(id):
-    if randint(0,1) == 0:
-        return True
-    else:
-        return False
+# Validation
+def valid_user(username):
+    return username == USERNAME
 
+def valid_collection(collection):
+    return collection == COLLECTION['id']
+
+# Utilities
 def id_from_title(title):
     p = re.compile('[\s]')
     return p.sub('-', title)
@@ -74,5 +119,11 @@ def id_from_title(title):
 def title_from_id(id):
     p = re.compile('[-]')
     return p.sub(' ', id)
+
+def get_recording_by_id(id):
+    if len(list(filter((lambda rec: id == rec['id']), COLLECTION['recordings']))) == 0:
+        return None
+    else:
+        return list(filter((lambda rec: id == rec['id']), COLLECTION['recordings']))[0]
 
 run(host='localhost', port=8080, debug=True)
