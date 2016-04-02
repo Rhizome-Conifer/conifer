5d7edf1ce8e788d50bd61c4e9ff8452d30a0d68e
Ilya Kreymer <ikreymer@gmail.com>
2016-04-02 15:00:00 -0700

    wragg: simplify path names

--- webagg/wragg.py
+++ webagg/wragg.py
@@ -6,10 +6,11 @@ from webagg.indexsource import LiveIndexSource, RedisIndexSource
 from webagg.aggregator import SimpleAggregator, RedisMultiKeyIndexSource
 
 import os
+
 from pywb.utils.loaders import load_yaml_config
 
 
-# ============================================================================
+# =============================================================================
 def make_webagg():
     config = load_yaml_config(os.environ.get('WR_CONFIG', './wr.yaml'))
 
@@ -17,9 +18,9 @@ def make_webagg():
 
     redis_base = os.environ['REDIS_BASE_URL'] + '/'
 
-    cdxj_key_templ = config['cdxj_key_templ']
-    cdxj_coll_key_templ = config['cdxj_coll_key_templ']
-    warc_key_templ = config['warc_key_templ']
+    rec_url = redis_base + config['cdxj_key_templ']
+    coll_url = redis_base + config['cdxj_coll_key_templ']
+    warc_url = redis_base + config['warc_key_templ']
 
 
     app.add_route('/live',
@@ -30,28 +31,28 @@ def make_webagg():
 
     app.add_route('/replay',
         DefaultResourceHandler(SimpleAggregator(
-                               {'replay': RedisIndexSource(redis_base + cdxj_key_templ)}),
-                                redis_base + warc_key_templ
+                               {'replay': RedisIndexSource(rec_url)}),
+                                warc_url
         )
     )
 
     app.add_route('/replay-coll',
         DefaultResourceHandler(SimpleAggregator(
-                               {'replay': RedisMultiKeyIndexSource(redis_base + cdxj_coll_key_templ)}),
-                                redis_base + warc_key_templ
+                               {'replay': RedisMultiKeyIndexSource(coll_url)}),
+                                warc_url
         )
     )
 
     return app.application
 
 
-
 application = make_webagg()
 
-import bottle
-bottle.debug = True
 
 if __name__ == "__main__":
+    from bottle import debug
+    debug(True)
+
     from gevent.wsgi import WSGIServer
     server = WSGIServer(('', 8080), application)
     server.serve_forever()
