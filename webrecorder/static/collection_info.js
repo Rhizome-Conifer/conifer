4c65a1ddea3644a362dc52cad8270b4561f92eae
Masha Lifshin <mlifshin@gmail.com>
2016-06-06 18:47:46 -0700

    add two panel layout and recording selection behavior

--- webrecorder/static/collection_info.js
+++ webrecorder/static/collection_info.js
@@ -1,14 +1,59 @@
 $(function() {
-    DataTables.start();
+    PublicPrivateSwitch.start();
+    BookmarksTable.start();
+    RecordingSelector.start();
+})
 
-    if (!can_admin) {
-        // Public/Private
-        return;
+var RecordingSelector = (function() {
+
+    var toggleRecordingSelection = function(event) {
+        if (isAllRecordingsCard(this)) {
+            $('.card-selected').removeClass('card-selected');
+            $(this).addClass('card-selected');
+        } else {
+            $('.recording-selector-panel').find('[data-recording-id="$all"]').removeClass('card-selected');
+            $(this).toggleClass('card-selected');
+        }
+
+        var recordingIds = $('.card-selected').map( function(){ return $(this).attr('data-recording-id') });
+
+        BookmarksTable.filterByRecordings(recordingIds);
+        updateRecordingFilterList(recordingIds);
     }
 
-    $(".ispublic").bootstrapSwitch();
+    var updateRecordingFilterList = function(recordingIds) {
+        var recordingList = "";
 
-    $(".ispublic").on('switchChange.bootstrapSwitch', function(event, state) {
+        if (recordingIds[0] === "$all") {
+            recordingList = "All recordings";
+        } else {
+            $.each(recordingIds, function() {
+                var title = $('.recording-selector-panel').find('[data-recording-id="' + this + '"]').attr('data-recording-title');
+                recordingList += title;
+                recordingList += ", ";
+            });
+            recordingList = recordingList.replace(/(,\s*$)/g, '');
+        }
+
+        $('.recording-filter-list').text(recordingList);
+    }
+
+    var isAllRecordingsCard = function(element) {
+        return $(element).attr('data-recording-id') === "$all";
+    }
+
+    var start = function() {
+        $('.recording-selector-panel').on('click', '.card', toggleRecordingSelection);
+    }
+
+    return {
+        start: start
+    }
+})();
+
+var PublicPrivateSwitch = (function() {
+
+    var updatePermission = function(event, state) {
         $.ajax({
             url: "/api/v1/collections/" + coll + "/public?user=" + user,
             method: "POST",
@@ -23,33 +68,22 @@ $(function() {
                 console.log("err");
             }
         });
-    });
-    
-    $("#update-title-form").submit(function(e) {
-        var title = $("#new-title").val();
-        var query = $.param({coll: coll, title: title});
-        
-        if (!title) {
-            return;
+    }
+
+    var start = function() {
+        if (can_admin) {
+            $(".ispublic").bootstrapSwitch();
+
+            $(".ispublic").on('switchChange.bootstrapSwitch', updatePermission);
         }
-        
-        $.ajax({
-            type: "GET",
-            url: "/_settitle?" + query,
-            success: function() {
-                console.log("success");
-                $("#coll-title").text(title);
-            },
-            error: function() {
-                console.log("err");
-            },
-            dataType: 'text',
-        });
-        e.preventDefault();
-    });
-});
- 
-var DataTables = (function() {
+    }
+
+    return {
+        start: start
+    }
+})();
+
+var BookmarksTable = (function() {
 
     var theTable;
 
@@ -67,8 +101,13 @@ var DataTables = (function() {
         }
     }
 
+    var filterByRecordings = function(recordingIds) {
+        console.log("filtering:" + recordingIds);
+    }
+
     return {
-        start: start
+        start: start,
+        filterByRecordings: filterByRecordings
     }
 
 })();
