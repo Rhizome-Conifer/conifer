38dcee3a4257a5e9a16c75be4644288d89d989e4
Ilya Kreymer <ikreymer@gmail.com>
2016-08-03 08:28:59 +0000

    migration: update migration script, support for size increment, timestamp updating, separate recording for snapshots (#53)

--- migration_scripts/migrate3.0.py
+++ migration_scripts/migrate3.0.py
@@ -27,7 +27,8 @@ from six.moves.urllib.parse import urlsplit
 from tempfile import NamedTemporaryFile
 
 from pywb.warc.cdxindexer import write_cdx_index
-from pywb.utils.timeutils import timestamp20_now
+from pywb.utils.timeutils import timestamp20_now, timestamp_to_sec
+from pywb.cdx.cdxobject import CDXObject
 
 
 class Migrater(object):
@@ -37,7 +38,7 @@ class Migrater(object):
 
     MIN_SIZE = 5000000000
 
-    NEW_REMOTE_PATH = 'accounts/{0}/warcs/migrate-{0}-{1}-{2}.warc.gz'
+    NEW_REMOTE_PATH = 'accounts/{0}/warcs/migrate-{0}-{1}-{2}{3}.warc.gz'
 
     def __init__(self, src_redis_url, dst_redis_url, dest_prefix, dry_run=True):
         self.src_redis = redis.StrictRedis.from_url(src_redis_url, decode_responses=True)
@@ -52,19 +53,32 @@ class Migrater(object):
 
         self.dest_bucket = self.s3conn.get_bucket(self.dest_bucket_name)
 
+    def process_all_users(self):
+        users = self.src_redis.hkeys('h:users')
+        for user in users:
+            self.process_user(user)
+
     def add_h_tables(self):
         for key in self.src_redis.scan_iter('h:*'):
             print(key)
 
+            if self.dst_redis.exists(key):
+                continue
+
             if key == 'h:reports':
                 values = self.src_redis.lrange(key, 0, -1)
-                #TODO
-                print(type(values))
+                if self.dry:
+                    continue
+
+                for val in values:
+                    self.dst_redis.rpush(key, val)
 
             else:
                 values = self.src_redis.hgetall(key)
-                if not self.dry:
-                    self.dst_redis.hmset(key, values)
+                if self.dry:
+                    continue
+
+                self.dst_redis.hmset(key, values)
 
     def add_user(self, user):
         from_key = 'u:{0}'.format(user)
@@ -84,10 +98,9 @@ class Migrater(object):
         new_info['desc'] = userinfo.get('desc', '')
         new_info['max_size'] = max_size
         new_info['size'] = '0'
-        new_info['max_coll'] = '100'
+        new_info['max_coll'] = '1000'
 
-        print('USER ' + to_key)
-        print(new_info)
+        print('USER: ' + to_key)
         if not self.dry:
             self.dst_redis.hmset(to_key, new_info)
 
@@ -102,6 +115,7 @@ class Migrater(object):
         collection['title'] = coll
         collection['id'] = coll
         collection['created_at'] = int(time.time())
+        collection['size'] = '0'
 
         # Collection Info
         if 'desc' not in collection:
@@ -112,30 +126,32 @@ class Migrater(object):
             collection['r:@public'] = '1'
 
         print('  COLL: ' + to_key)
-        print(collection)
         if not self.dry:
             self.dst_redis.hmset(to_key, collection)
 
         return orig_title
 
-    def add_default_rec(self, user, coll, rec, title):
+    def add_rec(self, user, coll, rec, title):
         key = 'r:{0}:{1}:{2}:info'.format(user, coll, rec)
 
         now = int(time.time())
 
         # create default recording
         recording = dict(id=rec,
-                         title=title,
-                         size='0',
-                         created_at=now,
-                         updated_at=now)
+                         title=title)
 
         print('  REC: ' + key)
-        print(recording)
-        if not self.dry:
-            self.dst_redis.hmset(key, recording)
+        if self.dry:
+            return
 
-    def add_pages(self, user, coll, rec):
+        if not self.dst_redis.exists(key):
+            recording['size'] = '0'
+            recording['updated_at'] = now
+            recording['created_at'] = now
+
+        self.dst_redis.hmset(key, recording)
+
+    def add_pages(self, user, coll, rec, is_snapshot):
         from_key = '{0}:{1}:p'.format(user, coll)
         to_key = 'r:{0}:{1}:{2}:page'.format(user, coll, rec)
 
@@ -145,17 +161,23 @@ class Migrater(object):
         for page in pages:
             page = json.loads(page)
 
+            if (page.get('tags') == ['snapshot']) != is_snapshot:
+                continue
+
             timestamp = page.pop('ts', '0')
             page_key = page['url'] + ' ' + timestamp
             page['timestamp'] = timestamp
 
             new_pages[page_key] = json.dumps(page)
 
-        print('  PAGES ' + to_key)
-        print(new_pages)
-        if not self.dry:
-            if new_pages:
-                self.dst_redis.hmset(to_key, new_pages)
+        print('  PAGES: ' + str(len(new_pages)))
+
+        #print(new_pages)
+        if self.dry:
+            return
+
+        if new_pages:
+            self.dst_redis.hmset(to_key, new_pages)
 
     def process_user(self, user):
         user_key = self.add_user(user)
@@ -175,37 +197,64 @@ class Migrater(object):
         # Recording Info
         rec = self.sanitize_title(title)
 
-        self.add_default_rec(user, coll, rec, title)
+        self.add_rec(user, coll, rec, title)
 
 
         # Pages
-        self.add_pages(user, coll, rec)
+        self.add_pages(user, coll, rec, False)
 
 
-        # Warcs
-        self.process_warcs(user, coll, rec)
+        # Warcs (non Snapshots)
+        self.process_warcs(user, coll, rec, False)
 
-    def process_warcs(self, user, coll, rec):
+        # Check for snapshots
+        rec_static_snap = rec + '-static-snapshots'
+
+        # Warcs for Snapshots
+        if self.process_warcs(user, coll, rec_static_snap, True) > 0:
+            print('  HAS SNAPSHOTS')
+            self.add_rec(user, coll, rec_static_snap, title + ' Static Snapshots')
+            self.add_pages(user, coll, rec_static_snap, True)
+
+
+    def process_warcs(self, user, coll, rec, is_snapshot):
         from_key = '{0}:{1}:warc'.format(user, coll)
         to_key = 'r:{0}:{1}:{2}:warc'.format(user, coll, rec)
 
         warcs = self.src_redis.hgetall(from_key)
 
-        print('  WARCS ' + to_key)
+        print('  WARCS: ' + to_key)
 
-        remote_path = self.NEW_REMOTE_PATH.format(user, coll, timestamp20_now())
+        if is_snapshot:
+            suffix = '-snapshot'
+        else:
+            suffix = ''
+
+        remote_path = self.NEW_REMOTE_PATH.format(user, coll, timestamp20_now(), suffix)
 
         remote_path = self.path_prefix + remote_path
 
         full_path = 's3://{0}/{1}'.format(self.dest_bucket_name, remote_path)
-        print('Uploading ' + full_path)
+
+        size = 0
 
         with NamedTemporaryFile() as fh:
             for name, path in iteritems(warcs):
-                self.append_warc(fh, path)
-                print(name, fh.tell())
+                if name.endswith('-snapshot.warc.gz') != is_snapshot:
+                    continue
 
-            print('  TOTAL ', fh.tell())
+                try:
+                    self.append_warc(fh, path)
+                    print('    {0}: {1}'.format(name, fh.tell()))
+                except:
+                    print('ERROR on ' + path)
+
+            size = fh.tell()
+            if not size:
+                return size
+
+            print('  TOTAL: ' + str(size))
+            print('  Uploading ' + full_path)
 
             # UPLOAD
             if not self.dry:
@@ -217,17 +266,35 @@ class Migrater(object):
 
                 dest_key.set_contents_from_file(fh, replace=True)
 
-
             # CDXJ
             z_key = 'r:{0}:{1}:{2}:cdxj'.format(user, coll, rec)
             filename = os.path.basename(remote_path)
             fh.seek(0)
-            self.cdx_index(z_key, fh, filename)
+
+            min_, max_ = self.cdx_index(z_key, fh, filename)
+
+            if self.dry:
+                return
 
             # SET WARC
             warc_key = 'r:{0}:{1}:{2}:warc'.format(user, coll, rec)
-            if not self.dry:
-                self.dst_redis.hset(warc_key, filename, full_path)
+            self.dst_redis.hset(warc_key, filename, full_path)
+
+            # SIZE INC + TIME SET
+            with redis.utils.pipeline(self.dst_redis) as pi:
+                pi.hincrby('u:{0}:info'.format(user), 'size', size)
+                pi.hincrby('c:{0}:{1}:info'.format(user, coll), 'size', size)
+
+                rec_key = 'r:{0}:{1}:{2}:info'.format(user, coll, rec)
+                pi.hincrby(rec_key, 'size', size)
+
+                if min_:
+                    pi.hset(rec_key, 'created_at', min_)
+
+                if max_:
+                    pi.hset(rec_key, 'updated_at', max_)
+
+        return size
 
     def cdx_index(self, z_key, stream, filename):
         cdxout = BytesIO()
@@ -237,16 +304,32 @@ class Migrater(object):
         cdx_list = cdxout.getvalue().rstrip().split(b'\n')
         count = 0
 
+        min_ = max_ = None
+
         for cdx in cdx_list:
             if cdx and not self.dry:
                 self.dst_redis.zadd(z_key, 0, cdx)
+                cdxobj = CDXObject(cdx)
+
+                ts = cdxobj['timestamp']
+
+                min_ = min(min_, ts) if min_ else ts
+                max_ = max(max_, ts) if max_ else ts
+
                 count += 1
 
-        print('  CDXJ ', count)
+        if count:
+            min_ = timestamp_to_sec(min_)
+            max_ = timestamp_to_sec(max_)
 
-
+        print('  CDXJ: ', count, min_, max_)
+        return min_, max_
 
     def append_warc(self, dest, path):
+        if not path.startswith('s3://'):
+            print('SKIPPING INVALID ' + path)
+            return
+
         parts = urlsplit(path)
 
         src_bucket = self.s3conn.get_bucket(parts.netloc)
@@ -276,9 +359,11 @@ def main():
     m = Migrater(src, dst,
                  os.environ['DEST_PREFIX'], dry_run=False)
 
-    #m.add_h_tables()
+    m.add_h_tables()
 
-    #m.process_user('<username>')
+    #m.process_all_users()
+
+    m.process_user('fkneist')
 
 
 main()
