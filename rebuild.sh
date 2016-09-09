0399c4f1662578bb39fc342eb83ccdaaa5182813
Ilya Kreymer <ikreymer@gmail.com>
2016-09-09 17:30:49 +0000

    fix rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # redeploy just webrecorder and nginx
-docker-compose build webrecorder;
-docker-compose stop webrecorder;
+docker-compose build app;
+docker-compose stop app;
 docker-compose stop nginx;
 
-docker-compose up -d --no-deps webrecorder nginx
+docker-compose up -d --no-deps app nginx
