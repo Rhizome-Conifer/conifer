aac8fd4a3c38b4b1d87013841a627302844c7097
Ilya Kreymer <ikreymer@gmail.com>
2016-10-19 10:53:53 -0700

    rewriter: check for malformed status line, add a 'none' reason if none present

--- webrecorder/webrecorder/contentcontroller.py
+++ webrecorder/webrecorder/contentcontroller.py
@@ -247,14 +247,14 @@ class ContentController(BaseController, RewriterApp):
 
             resp = self.render_content(wb_url, kwargs, request.environ)
 
-            #self._filter_headers(type, resp.status_headers)
-
-            # Checks if this is a containerized replay, injecting strict no-cache rules
-            # to prevent browser history caching.
-            #self._inject_nocache_headers(resp.status_headers, kwargs)
+            # check for invalid status line
+            # TODO: move this upstream into rewriter?
+            status = resp.status_headers.statusline
+            if ' ' not in status:
+                status += ' None'
 
             resp = HTTPResponse(body=resp.body,
-                                status=resp.status_headers.statusline,
+                                status=status,
                                 headers=resp.status_headers.headers)
 
             return resp
