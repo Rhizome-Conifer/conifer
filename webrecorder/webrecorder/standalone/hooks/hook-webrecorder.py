fd34f8780aa50d7b9b6a7a4783a2a823d9009ec5
Ilya Kreymer <ikreymer@gmail.com>
2016-12-28 05:04:57 +0000

    standalone improvements: make hook-webrecorder more cross-platform, use path sep and path join for dealing with paths (#282)

--- webrecorder/webrecorder/standalone/hooks/hook-webrecorder.py
+++ webrecorder/webrecorder/standalone/hooks/hook-webrecorder.py
@@ -14,10 +14,18 @@ build(assets_path)
 
 datas = []
 
+# special package to put templates into to allow pyinstaller pkg_resources to find them
+temp_pkg = 'wrtemp'
+
 # move templates to separate 'wrtemp' package (only for bundled apps) to avoid
 # issue with loading
-datas.append((os.path.abspath('../__init__.py'), 'wrtemp'))
-datas += rename('webrecorder/', 'wrtemp/', collect_data_files('webrecorder', subdir='templates'))
+init_py_path = os.path.abspath(os.path.join(curr_path, '..', '..', '__init__.py'))
+
+datas.append((init_py_path, temp_pkg))
+
+datas += rename('webrecorder' + os.path.sep,
+                temp_pkg + os.path.sep,
+                collect_data_files('webrecorder', subdir='templates'))
 
 datas += collect_data_files('webrecorder', subdir='static')
 datas += collect_data_files('webrecorder', subdir='config')
