ac2de11c32331f9278ea53ffff48137f67fd4744
Mark Beasley <mbeasl1@gmail.com>
2017-03-31 16:28:53 -0400

    Add news alert box to homepage

--- webrecorder/webrecorder/static/user.js
+++ webrecorder/webrecorder/static/user.js
@@ -2,6 +2,14 @@ $(function(){
 
     $(".ispublic").bootstrapSwitch();
 
+    $('#news-alert').on('close.bs.alert', function () {
+        setStorage("__wr_skipNews", "1");
+    });
+
+    if(getStorage("__wr_skipNews") === "1") {
+      $('#news-alert').hide();
+    }
+
     $('#create-modal').on('shown.bs.modal', function () {
         $('#title').select();
     });
