0fffb7411183fab48b293dbaeb9074061be9c39b
Mark Beasley <mbeasl1@gmail.com>
2017-08-01 21:24:18 -0400

    left out site components index

---
+++ frontend/src/components/SiteComponents/index.js
@@ -0,0 +1,7 @@
+export Breadcrumbs from './Breadcrumbs';
+export HomepageAnnouncement from './HomepageAnnouncement';
+export HomepageMessage from './HomepageMessage';
+export FAQ from './FAQ';
+export Footer from './Footer';
+export TermsAndPolicies from './TermsAndPolicies';
+export UserManagementUI from './UserManagementUI';
