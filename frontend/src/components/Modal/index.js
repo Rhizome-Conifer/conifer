a1b2095bde421940559dda212769ca333a0a5e8a
Mark Beasley <mbeasl1@gmail.com>
2017-10-02 11:57:31 -0400

    Shim for react-bootstrap modals

--- frontend/src/components/Modal/index.js
+++ frontend/src/components/Modal/index.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
+import { Modal as ReactOverlayModal } from 'react-overlays';
 import { Modal as BSModal } from 'react-bootstrap';
+
+
+const focus = () => {};
+const cDU = ReactOverlayModal.prototype.componentDidUpdate;
+const cDM = ReactOverlayModal.prototype.componentDidMount;
+
+ReactOverlayModal.prototype.componentDidUpdate = function (prevProps: any) {
+  if (this.focus !== focus) {
+    this.focus = focus;
+  }
+  cDU.call(this, prevProps);
+};
+
+ReactOverlayModal.prototype.componentDidMount = function () {
+  if (this.focus !== focus) {
+    this.focus = focus;
+  }
+  cDM.call(this);
+};
 
 
 class Modal extends Component {
