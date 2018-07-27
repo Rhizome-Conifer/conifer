03b11c46f7ebcc85201c9c9a0ffcb5209c55e346
Mark Beasley <mbeasl1@gmail.com>
2018-07-27 16:51:24 -0400

    Add dat entry field, source display

--- frontend/src/containers/Landing/index.js
+++ frontend/src/containers/Landing/index.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
+import { batchActions } from 'redux-batched-actions';
+import { Button, FormControl} from 'react-bootstrap';
 
-import { setHost } from 'redux/modules/appSettings';
+import { setHost, setSource } from 'redux/modules/appSettings';
 import { rts } from 'helpers/utils';
 import { openFile } from 'helpers/playerUtils';
 
@@ -22,15 +24,32 @@ class Landing extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { initializing: false };
+    this.state = {
+      datUrl: '',
+      initializing: false
+    };
   }
 
   componentDidMount() {
     ipcRenderer.once('initializing', () => this.setState({ initializing: true }));
     ipcRenderer.once('indexing', (evt, data) => {
-      this.props.dispatch(setHost(rts(data.host)));
-      this.props.history.push('/indexing');
+      this.props.dispatch(batchActions([
+        setHost(rts(data.host)),
+        setSource(data.source)
+      ]));
+
+      this.props.history.push(data.source.startsWith('dat') ? '/local/collection' : '/indexing');
     });
+  }
+
+  handleInput = (evt) => {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+
+  syncDat = () => {
+    if (this.state.datUrl) {
+      ipcRenderer.send('sync-dat', this.state.datUrl);
+    }
   }
 
   render() {
@@ -41,11 +60,20 @@ class Landing extends Component {
         {
           initializing ?
             <img src={require('shared/images/loading.svg')} id="loadingGif" alt="loading" /> :
-            <div className="bigOpen">
-              <button onClick={openFile}>
-                <object id="loadWarc" data={require('shared/images/Load_WARC.svg')} type="image/svg+xml">load</object>
-              </button>
-            </div>
+            <React.Fragment>
+              <div className="bigOpen">
+                <button onClick={openFile}>
+                  <object id="loadWarc" data={require('shared/images/Load_WARC.svg')} type="image/svg+xml">load</object>
+                </button>
+              </div>
+              <div className="dat-url">
+                Or via dat:
+                <div className="dat-input">
+                  <FormControl type="text" name="datUrl" onChange={this.handleInput} value={this.state.datUrl} placeholder="Enter a dat:// url" />
+                  <Button bsStyle="primary" onClick={this.syncDat}>Sync</Button>
+                </div>
+              </div>
+            </React.Fragment>
         }
 
         <div className="projectByRhizome">
