1833c34e40c240069f3cba916795bb0f6ef48d69
aarati <aarati.akkapeddi@gmail.com>
2018-08-02 17:06:41 -0400

    fixes blink state

--- frontend/src/components/Blinker/index.js
+++ frontend/src/components/Blinker/index.js
@@ -5,7 +5,6 @@ import { CSSTransitionGroup } from 'react-transition-group';
 
 
 import './style.scss';
-//<span onLoad={this.blinkAnimation()} className="glyphicon glyphicon-dot-sm glyphicon-recording-status Blink" aria-hidden="true" />
 
 class Blinker extends Component {
   static propTypes = {
@@ -14,21 +13,18 @@ class Blinker extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { blink: "meow" };
+    this.state = { blink: true };
   }
 
   componentDidUpdate(prevProps, prevState) {
     if(this.props.dataPing && !prevProps.dataPing) {
       this.setState({ blinking: true });
-      // clear blink timeout if another request comes in before previous blink is done
       clearTimeout(this.blinkHandle);
-      // blink anywhere from 500ms - 1.5s
       this.blinkHandle = setTimeout(() => this.setState({ blink: false }), (Math.random() * 1000) + 500);
     }
   }
 
   componentWillUnmount() {
-    // make sure timer is cleared before unmounting
     clearTimeout(this.blinkHandle);
   }
 
