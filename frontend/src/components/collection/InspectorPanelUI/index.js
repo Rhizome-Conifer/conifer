23788cdbd00e786379281619f136eb58f8aa6954
aarati <aarati.akkapeddi@gmail.com>
2018-08-08 14:50:17 -0400

    only show description in inspector panel if present

--- frontend/src/components/collection/InspectorPanelUI/index.js
+++ frontend/src/components/collection/InspectorPanelUI/index.js
@@ -59,7 +59,7 @@ class InspectorPanelUI extends PureComponent {
     const bk = selectedBk ? list.get('bookmarks').find(o => o.get('id') === selectedBk) : false;
     const pg = bk ? bk.get('page') : collection.getIn(['pages', selectedPage]);
     const selectedIndex = selectedBk ? list.get('bookmarks').findIndex(o => o.get('id') === selectedBk) : null;
-
+    const descPresent = (bk && bk.get('desc')) ? bk.get('desc').trim() : '';
     return (
       <div className="wr-inspector">
         <SidebarHeader label="Metadata" />
@@ -82,7 +82,7 @@ class InspectorPanelUI extends PureComponent {
                         <h2>{bk.get('title')}</h2>
                       </InlineEditor>
 
-                      <h4>Description</h4>
+                      <h4>{((descPresent.length < 1) && !canAdmin) ? '' : 'Description'}</h4>
                       <WYSIWYG
                         clickToEdit
                         initial={bk.get('desc')}
