83e586999dfb4d6fd477263df1c2afdb4f2ebc26
Carine Dengler <carine-dengler@zo.uni-heidelberg.de>
2018-08-08 11:32:45 +0200

    apiutils.py
    - adapted to coding convention

--- webrecorder/webrecorder/apiutils.py
+++ webrecorder/webrecorder/apiutils.py
@@ -1,8 +1,11 @@
-import datetime
+# standard library imports
 import json
+import datetime
 
 
 class CustomJSONEncoder(json.JSONEncoder):
+    """Customized JSON encoder."""
+
     def default(self, obj):
         if isinstance(obj, datetime.datetime):
             return str(obj.strftime("%Y-%m-%d %H:%M:%S"))
