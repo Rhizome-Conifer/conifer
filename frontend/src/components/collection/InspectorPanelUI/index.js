86bd3fe62d794c1071d41cbfd00ece1bc5b6e7fa
aarati <aarati.akkapeddi@gmail.com>
2018-08-14 11:16:58 -0400

    makes metadata in inspector collapsible

--- frontend/src/components/collection/InspectorPanelUI/index.js
+++ frontend/src/components/collection/InspectorPanelUI/index.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Collapsible from 'react-collapsible';
 
 import { defaultBookmarkDesc, untitledEntry } from 'config';
 import { getCollectionLink } from 'helpers/utils';
@@ -62,89 +63,90 @@ class InspectorPanelUI extends PureComponent {
 
     return (
       <div className="wr-inspector">
-        <SidebarHeader label="Metadata" />
-        <div className="inspector-body">
-          {
-            multiSelect ?
-              <h5>{multiSelect} Pages Selected</h5> :
-              <React.Fragment>
-                {
-                  bk &&
-                    <header className="bookmark-cap">
-                      <h4><BookmarkIcon /> Bookmark {selectedIndex + 1} of {list.get('bookmarks').size}</h4>
+        <Collapsible trigger="Metadata" >
+          <div className="inspector-body">
+            {
+              multiSelect ?
+                <h5>{multiSelect} Pages Selected</h5> :
+                <React.Fragment>
+                  {
+                    bk &&
+                      <header className="bookmark-cap">
+                        <h4><BookmarkIcon /> Bookmark {selectedIndex + 1} of {list.get('bookmarks').size}</h4>
 
-                      <InlineEditor
-                        blockDisplay
-                        initial={bk.get('title') || untitledEntry}
-                        onSave={this.editBookmarkTitle}
-                        readOnly={!canAdmin}
-                        success={bkEdited}>
-                        <h2>{bk.get('title')}</h2>
-                      </InlineEditor>
+                        <InlineEditor
+                          blockDisplay
+                          initial={bk.get('title') || untitledEntry}
+                          onSave={this.editBookmarkTitle}
+                          readOnly={!canAdmin}
+                          success={bkEdited}>
+                          <h2>{bk.get('title')}</h2>
+                        </InlineEditor>
 
-                      {
-                        (bk.get('desc') || canAdmin) &&
-                          <React.Fragment>
-                            <h4>Description</h4>
-                            <WYSIWYG
-                              clickToEdit
-                              initial={bk.get('desc')}
-                              placeholder="Add annotation"
-                              readOnly={!canAdmin}
-                              onSave={this.editBookmarkDesc}
-                              success={bkEdited} />
-                          </React.Fragment>
-                      }
-                    </header>
-                }
-                {
-                  pg ?
-                    <div className="page-metadata">
-                      <h4><InfoIcon /> Page Properties</h4>
-                      <ul>
-                        <li>
-                          <h5>Page Title</h5>
-                          <span className="value">{pg.get('title')}</span>
-                        </li>
-                        <li>
-                          <h5>Page Url</h5>
-                          <span className="value">
-                            <Link to={`${getCollectionLink(collection)}/${pg.get('timestamp')}/${pg.get('url')}`}>{pg.get('url')}</Link>
-                          </span>
-                        </li>
-                        <li>
-                          <h5>Captured At</h5>
-                          <span className="value">
-                            <TimeFormat dt={pg.get('timestamp')} />
-                          </span>
-                        </li>
                         {
-                          pg.get('rec') &&
+                          (bk.get('desc') || canAdmin) &&
+                            <React.Fragment>
+                              <h4>Description</h4>
+                              <WYSIWYG
+                                clickToEdit
+                                initial={bk.get('desc')}
+                                placeholder="Add annotation"
+                                readOnly={!canAdmin}
+                                onSave={this.editBookmarkDesc}
+                                success={bkEdited} />
+                            </React.Fragment>
+                        }
+                      </header>
+                  }
+                  {
+                    pg ?
+                      <div className="page-metadata">
+                        <h4><InfoIcon /> Page Properties</h4>
+                        <ul>
                           <li>
-                            <h5>Session ID</h5>
+                            <h5>Page Title</h5>
+                            <span className="value">{pg.get('title')}</span>
+                          </li>
+                          <li>
+                            <h5>Page Url</h5>
                             <span className="value">
-                              {
-                                canAdmin ?
-                                  <Link to={`${getCollectionLink(collection)}/management?session=${pg.get('rec')}`}>{pg.get('rec')}</Link> :
-                                  <Link to={`${getCollectionLink(collection, true)}?query=session:${pg.get('rec')}`}>{pg.get('rec')}</Link>
-                              }
+                              <Link to={`${getCollectionLink(collection)}/${pg.get('timestamp')}/${pg.get('url')}`}>{pg.get('url')}</Link>
                             </span>
                           </li>
-                        }
-                        {
-                          pg.get('browser') &&
                           <li>
-                            <h5>Preconfigured Browser</h5>
-                            <span className="value"><RemoteBrowserDisplay browser={browsers.getIn(['browsers', pg.get('browser')])} /></span>
+                            <h5>Captured At</h5>
+                            <span className="value">
+                              <TimeFormat dt={pg.get('timestamp')} />
+                            </span>
                           </li>
-                        }
-                      </ul>
-                    </div> :
-                    <h5><em>Select a page to see metadata</em></h5>
-                }
-              </React.Fragment>
-          }
-        </div>
+                          {
+                            pg.get('rec') &&
+                            <li>
+                              <h5>Session ID</h5>
+                              <span className="value">
+                                {
+                                  canAdmin ?
+                                    <Link to={`${getCollectionLink(collection)}/management?session=${pg.get('rec')}`}>{pg.get('rec')}</Link> :
+                                    <Link to={`${getCollectionLink(collection, true)}?query=session:${pg.get('rec')}`}>{pg.get('rec')}</Link>
+                                }
+                              </span>
+                            </li>
+                          }
+                          {
+                            pg.get('browser') &&
+                            <li>
+                              <h5>Preconfigured Browser</h5>
+                              <span className="value"><RemoteBrowserDisplay browser={browsers.getIn(['browsers', pg.get('browser')])} /></span>
+                            </li>
+                          }
+                        </ul>
+                      </div> :
+                      <h5><em>Select a page to see metadata</em></h5>
+                  }
+                </React.Fragment>
+            }
+          </div>
+        </Collapsible>
       </div>
     );
   }
