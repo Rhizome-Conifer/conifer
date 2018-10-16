60165d6ece64c03044bc5518b7af7f0a18d291c6
Ilya Kreymer <ikreymer@gmail.com>
2018-10-16 13:04:37 -0700

    tests: add additional test for not found collection

--- webrecorder/test/test_app_content_domain.py
+++ webrecorder/test/test_app_content_domain.py
@@ -427,6 +427,42 @@ class TestAppContentDomain(FullStackTests):
                                         'Referer': refer_url},
                                status=404)
 
+    def test_not_found_coll_no_cookie(self):
+        # clear content cookie
+        self.testapp.cookiejar.clear(domain='content-host.local')
+
+        url = '/test/default-collection-invalid/mp_/http://httpbin.org/get?bood=far'
+        refer_url = 'http://app-host' + url.replace('mp_/', '')
+
+        res = self.testapp.get(url,
+                               headers={'Host': 'content-host',
+                                        'Referer': refer_url},
+                               status=302)
+
+        assert 'content_cookie' not in res.headers['Location']
+        assert 'Set-Cookie' not in res.headers
+
+        # app /_set_session
+        res = self.app_get(res.headers['Location'])
+
+        assert 'Set-Cookie' not in res.headers
+
+        # content /_set_session
+        res = self.content_get(res.headers['Location'])
+
+        assert res.headers['Location'].endswith(url)
+
+        res = self.testapp.get(res.headers['Location'],
+                               headers={'Host': 'content-host',
+                                        'Referer': refer_url},
+                               status=307)
+
+        assert 'app-host' in res.headers['Location']
+        res = self.app_get(res.headers['Location'])
+
+        assert 'content-host' in res.headers['Location']
+        res = self.content_get(res.headers['Location'], status=404)
+
     def test_public_no_cookie_needed(self):
         params = {'public': True,
                  }
