c9afaffdc91fbe220391fa9ded141af760ab97fe
Ilya Kreymer <ikreymer@gmail.com>
2018-11-06 14:22:20 -0800

    rate limits: add 'rate-unlimited-archivist' role which skips the rate limit - don't increment rate counter if ignoring rate limit (for admin or rate-unlimited-archivis) - User.is_rate_limited() returns False is not limited, None if limit is being skipped - tests: ensure rate limit ignored for rate-unlimited-archivist, and rate counter not incremented

--- webrecorder/test/test_rate_limits.py
+++ webrecorder/test/test_rate_limits.py
@@ -2,6 +2,8 @@ from .testutils import FullStackTests
 import os
 import time
 
+from webrecorder.models.usermanager import CLIUserManager
+
 
 # ============================================================================
 class TestRateLimits(FullStackTests):
@@ -16,6 +18,7 @@ class TestRateLimits(FullStackTests):
         os.environ['RATE_LIMIT_RESTRICTED_MAX'] = str(cls.RESTRICT_LIMIT)
         os.environ['RATE_LIMIT_RESTRICTED_IPS'] = '255.255.255,10.0.0'
         super(TestRateLimits, cls).setup_class()
+        cls.user_manager = CLIUserManager()
 
     @classmethod
     def teardown_class(cls):
@@ -60,7 +63,9 @@ class TestRateLimits(FullStackTests):
 
         time.sleep(0.0)
         keys = self.redis.keys('ipr:10.0.0*')
-        assert int(self.redis.get(keys[0])) >= self.RESTRICT_LIMIT
+
+        TestRateLimits.limit = int(self.redis.get(keys[0]))
+        assert TestRateLimits.limit >= self.RESTRICT_LIMIT
 
 
         res = self.testapp.get('/{user}/temp/rec/record/mp_/http://httpbin.org/get?food=bar5'.format(user=self.anon_user),
@@ -69,3 +74,57 @@ class TestRateLimits(FullStackTests):
 
         assert res.status_code == 402
 
+    def test_create_rate_unlimited(self):
+        self.user_manager.create_user('user2@example.com', 'test-unlim', 'TestTest456', 'rate-unlimited-archivist', 'Test User')
+
+        params = {'username': 'test-unlim',
+                  'password': 'TestTest456',
+                 }
+
+        res = self.testapp.post_json('/api/v1/auth/login', params=params)
+        assert res.json['user']['username'] == 'test-unlim'
+        assert self.testapp.cookies['__test_sesh'] != ''
+
+    def test_record_not_limited(self):
+        self.set_uuids('Recording', ['rec'])
+        headers = {'X-Real-IP': '10.0.0.5'}
+        res = self.testapp.get('/_new/temp/rec/record/mp_/http://httpbin.org/get?food=bar', headers=headers, status=302)
+        res = res.follow(headers=headers)
+
+        assert '"food": "bar"' in res.text, res.text
+
+        time.sleep(0.0)
+        keys = self.redis.keys('ipr:10.0.0*')
+
+        assert int(self.redis.get(keys[0])) == TestRateLimits.limit
+
+        res = self.testapp.post_json('/api/v1/auth/logout', status=200)
+        assert res.json['success']
+
+    def test_create_rate_limited(self):
+        self.user_manager.create_user('user@example.com', 'test-lim', 'TestTest456', 'archivist', 'Test User')
+
+        params = {'username': 'test-lim',
+                  'password': 'TestTest456',
+                 }
+
+        res = self.testapp.post_json('/api/v1/auth/login', params=params)
+        assert res.json['user']['username'] == 'test-lim'
+        assert self.testapp.cookies['__test_sesh'] != ''
+
+    def test_record_logged_in_limited(self):
+        self.set_uuids('Recording', ['rec'])
+        headers = {'X-Real-IP': '10.0.0.5'}
+
+        res = self.testapp.get('/_new/temp/rec/record/mp_/http://httpbin.org/get?food=bar5',
+                               headers=headers, status=302)
+
+        res = res.follow(headers=headers, status=402)
+
+        time.sleep(0.0)
+        keys = self.redis.keys('ipr:10.0.0*')
+
+        assert int(self.redis.get(keys[0])) == TestRateLimits.limit
+
+        res = self.testapp.post_json('/api/v1/auth/logout', status=200)
+        assert res.json['success']
