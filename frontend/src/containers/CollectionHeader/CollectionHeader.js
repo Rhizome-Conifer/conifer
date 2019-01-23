gitter-badger/webrecorder

Replace webpack-isomorphic with webpack-universal - rename `redux` -> `store`

frontend/src/containers/CollectionHeader/CollectionHeader.js
@@ -5,8 +5,8 @@ import { withRouter } from 'react-router';
 import { saveDelay } from 'config';
 
 import { resetEditState as resetCollEditState,
-         edit as editCollDesc } from 'redux/modules/collection';
-import { loadCollections } from 'redux/modules/user';
+         edit as editCollDesc } from 'store/modules/collection';
+import { loadCollections } from 'store/modules/user';
 
 import CollectionHeaderUI from 'components/collection/CollectionHeaderUI';
 

frontend/src/containers/Extract/Extract.js
@@ -6,12 +6,12 @@ import { batchActions } from 'redux-batched-actions';
 
 import config from 'config';
 
-import { isLoaded, load as loadColl } from 'redux/modules/collection';
-import { getArchives, setExtractable, updateUrlAndTimestamp } from 'redux/modules/controls';
-import { resetStats } from 'redux/modules/infoStats';
-import { load as loadBrowsers, isLoaded as isRBLoaded, setBrowser } from 'redux/modules/remoteBrowsers';
-import { getActiveCollection } from 'redux/selectors';
-import { selectCollection } from 'redux/modules/user';
+import { isLoaded, load as loadColl } from 'store/modules/collection';
+import { getArchives, setExtractable, updateUrlAndTimestamp } from 'store/modules/controls';
+import { resetStats } from 'store/modules/infoStats';
+import { load as loadBrowsers, isLoaded as isRBLoaded, setBrowser } from 'store/modules/remoteBrowsers';
+import { getActiveCollection } from 'store/selectors';
+import { selectCollection } from 'store/modules/user';
 
 import { RemoteBrowser } from 'containers';
 import { IFrame, ReplayUI } from 'components/controls';

Rhizome-Conifer/conifer

frontend/src/containers/CollectionHeader/CollectionHeader.js
@@ -4,10 +4,9 @@ import { withRouter } from 'react-router';
 
 import { saveDelay } from 'config';
 
-
-import { loadCollections } from 'redux/modules/auth';
+import { loadCollections } from 'store/modules/auth';
 import { resetEditState as resetCollEditState, edit as editCollDesc,
-         shareToDat, unshareFromDat } from 'redux/modules/collection';
+         shareToDat, unshareFromDat } from 'store/modules/collection';
 
 import CollectionHeaderUI from 'components/collection/CollectionHeaderUI';
 

frontend/src/containers/Extract/Extract.js
@@ -6,12 +6,12 @@ import { batchActions } from 'redux-batched-actions';
 
 import config from 'config';
 
-import { selectCollection } from 'redux/modules/auth';
-import { isLoaded, load as loadColl } from 'redux/modules/collection';
-import { getArchives, setExtractable, updateUrlAndTimestamp } from 'redux/modules/controls';
-import { resetStats } from 'redux/modules/infoStats';
-import { load as loadBrowsers, isLoaded as isRBLoaded, setBrowser } from 'redux/modules/remoteBrowsers';
-import { getActiveCollection } from 'redux/selectors';
+import { selectCollection } from 'store/modules/auth';
+import { isLoaded, load as loadColl } from 'store/modules/collection';
+import { getArchives, setExtractable, updateUrlAndTimestamp } from 'store/modules/controls';
+import { resetStats } from 'store/modules/infoStats';
+import { load as loadBrowsers, isLoaded as isRBLoaded, setBrowser } from 'store/modules/remoteBrowsers';
+import { getActiveCollection } from 'store/selectors';
 
 import { RemoteBrowser } from 'containers';
 import { IFrame, ReplayUI } from 'components/controls';