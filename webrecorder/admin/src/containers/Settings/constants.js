654ccaebba936414c4235ee2061e85ad8bc6d1b6
Mark Beasley <mbeasl1@gmail.com>
2019-02-12 15:14:46 -0500

    Remove old webrecorder admin

--- webrecorder/admin/src/containers/Settings/constants.js
+++
@@ -1,8 +0,0 @@
-import { actionKey } from 'config';
-
-const component = 'Settings';
-
-export const LOAD_SETTINGS = `${actionKey + component}/LOAD_SETTINGS`;
-export const LOAD_SETTINGS_SUCCESS = `${actionKey + component}/LOAD_SETTINGS_SUCCESS`;
-export const UPDATE_SETTINGS = `${actionKey + component}/UPDATE_SETTINGS`;
-export const UPDATE_SETTINGS_SUCCESS = `${actionKey + component}/UPDATE_SETTINGS_SUCCESS`;
--- webrecorder/admin/src/containers/UserDetail/constants.js
+++
@@ -1,8 +0,0 @@
-import { actionKey } from 'config';
-
-const component = 'UserDetail';
-
-export const LOAD_USER = `${actionKey + component}/LOAD_USER`;
-export const LOAD_USER_SUCCESS = `${actionKey + component}/LOAD_USER_SUCCESS`;
-export const UPDATE_USER = `${actionKey + component}/UPDATE_USER`;
-export const UPDATE_USER_SUCCESS = `${actionKey + component}/UPDATE_USER_SUCCESS`;
