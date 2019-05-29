9b66d2fb951a391d7ddc3e67ffd89a83cba0251e
Mark Beasley <mbeasl1@gmail.com>
2019-05-29 12:23:04 -0400

    a11y

--- frontend/src/components/siteComponents/SupporterAnnounce/index.js
+++ frontend/src/components/siteComponents/SupporterAnnounce/index.js
@@ -19,7 +19,7 @@ const SupporterAnnounce = React.memo(({ dismiss }) => {
       <div className="banner">
         <div className="wrapper">
           <h3>Help us provide free tools and services that enable anyone to archive web resources.</h3>
-          <button className="dismiss" onClick={dismiss} type="button"><XIcon /></button>
+          <button className="dismiss" ariaLabel="Dismiss Supporter Announcement" onClick={dismiss} type="button"><XIcon /></button>
         </div>
       </div>
       <div className="info">
