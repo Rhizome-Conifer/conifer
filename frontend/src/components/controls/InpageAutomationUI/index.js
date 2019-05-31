a35e92ee5cc199f4c3abdef31cd3c97cdf099629
Mark Beasley <mbeasl1@gmail.com>
2019-05-31 17:10:32 -0400

    Remove remote browser select

--- frontend/src/components/controls/InpageAutomationUI/index.js
+++ frontend/src/components/controls/InpageAutomationUI/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Dropdown, MenuItem } from 'react-bootstrap';
 
 import { WandIcon, ThinXIcon } from 'components/icons';
 
@@ -9,11 +8,8 @@ import './style.scss';
 
 class InpageAutomationUI extends Component {
   static propTypes = {
-    browsers: PropTypes.object,
     open: PropTypes.bool,
     running: PropTypes.bool,
-    selectedBrowser: PropTypes.string,
-    selectRemoteBrowser: PropTypes.func,
     toggleInpageAutomation: PropTypes.func
   };
 
@@ -29,19 +25,14 @@ class InpageAutomationUI extends Component {
     this.setState({ behavior: evt.target.value });
   }
 
-  selectBrowser = (browser) => {
-    this.props.selectRemoteBrowser(browser === 'native' ? null : browser);
-  }
-
   selectMode = (mode) => {
     this.setState({ behavior: mode });
   }
 
   startAutomation = () => {
     const { behavior } = this.state;
-    const { selectedBrowser } = this.props;
     if (behavior) {
-      console.log(`start behavior ${behavior} with browser: ${selectedBrowser || 'native'}`);
+      console.log(`start behavior ${behavior}`);
     }
   }
 
@@ -50,13 +41,11 @@ class InpageAutomationUI extends Component {
   }
 
   render() {
-    const { browsers, selectedBrowser } = this.props;
-
     return (
       <div className="inpage-sidebar">
         <h2>Capture Options <button onClick={this.toggle} type="button"><ThinXIcon /></button></h2>
         <h4><WandIcon /> Autopilot</h4>
-        <p>Capture the content on this page with a pre-scripted behavior running on a remote browser.</p>
+        <p>Capture the content on this page with a scripted behavior.</p>
         <ul className="behaviors">
           <li onClick={this.selectMode.bind(this, 'default')}>
             <input type="radio" name="behavior" value="default" aria-labelledby="opt1" onChange={this.handleInput} checked={this.state.behavior === 'default'} />
@@ -74,25 +63,6 @@ class InpageAutomationUI extends Component {
             </div>
           </li>
         </ul>
-
-        <div className="browser-selection">
-          <label className="browser-label" htmlFor="inpage-browser">Capture Browser:</label>
-          <Dropdown
-            onSelect={this.selectBrowser}
-            id="inpage-browser">
-            {
-              selectedBrowser ?
-                <Dropdown.Toggle><img src={`/api/browsers/browsers/${selectedBrowser}/icon`} alt="Browser Icon" />{`${browsers.getIn([selectedBrowser, 'name'])} v${browsers.getIn([selectedBrowser, 'version'])}`}</Dropdown.Toggle> :
-                <Dropdown.Toggle>Choose Browser</Dropdown.Toggle>
-            }
-            <Dropdown.Menu>
-              <MenuItem eventKey="native">Current Browser</MenuItem>
-              {
-                browsers.valueSeq().map(browser => <MenuItem key={browser.get('id')} eventKey={browser.get('id')}><img src={`/api/browsers/browsers/${browser.get('id')}/icon`} alt="Browser Icon" />{`${browser.get('name')} v${browser.get('version')}`}</MenuItem>)
-              }
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
 
         <button className="rounded" onClick={this.startAutomation} type="button">Start Autopilot</button>
       </div>
--- frontend/src/containers/InpageAutomation/InpageAutomation.js
+++ frontend/src/containers/InpageAutomation/InpageAutomation.js
@@ -2,23 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleInpageSidebar } from 'store/modules/automation';
-import { selectBrowser } from 'store/modules/remoteBrowsers';
 
 import { InpageAutomationUI } from 'components/controls';
 
 
 const mapStateToProps = ({ app }) => {
   return {
-    browsers: app.getIn(['remoteBrowsers', 'browsers']),
     running: app.getIn(['automation', 'inpageRunning']),
-    open: app.getIn(['automation', 'inpageAutomation']),
-    selectedBrowser: app.getIn(['remoteBrowsers', 'selectedBrowser'])
+    open: app.getIn(['automation', 'inpageAutomation'])
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    selectRemoteBrowser: br => dispatch(selectBrowser(br)),
     toggleInpageAutomation: b => dispatch(toggleInpageSidebar(b))
   };
 };
