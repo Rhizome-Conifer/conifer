ebbedbf4b89d34465bed6b13a7e3fde22390b1a6
Rebecca Cremona <rcremona@law.harvard.edu>
2019-05-31 14:06:25 -0400

    Option to silence debug logs from recorder and tempchecker.

--- webrecorder/webrecorder/rec/main.py
+++ webrecorder/webrecorder/rec/main.py
@@ -1,12 +1,15 @@
 from gevent import monkey; monkey.patch_all()
 
+import os
+from ast import literal_eval
+
 from webrecorder.utils import load_wr_config, init_logging, spawn_once
 from webrecorder.rec.webrecrecorder import WebRecRecorder
 
 
 # =============================================================================
 def init():
-    init_logging(debug=True)
+    init_logging(debug=literal_eval(os.environ.get('WR_DEBUG', 'True')))
 
     config = load_wr_config()
 
