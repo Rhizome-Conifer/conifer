rebeccacremona/webrecorder

Add DESKTOP to eslint globals, update conditional import, webpack mode

frontend/src/components/controls/RemoteBrowserUI/index.js
@@ -9,7 +9,7 @@ import { createRemoteBrowser } from 'store/modules/remoteBrowsers';
 
 import './style.scss';
 
-const CBrowser = !__PLAYER__ && __CLIENT__ && require('shepherd-client/lib/browser').default;
+const CBrowser = !__DESKTOP__ && !__PLAYER__ && __CLIENT__ && require('shepherd-client/lib/browser').default;
 
 
 class RemoteBrowserUI extends Component {

Rhizome-Conifer/conifer

frontend/src/components/controls/RemoteBrowserUI/index.js
@@ -11,7 +11,7 @@ import { publicIP } from 'config';
 
 import './style.scss';
 
-const CBrowser = !__PLAYER__ && __CLIENT__ && require('shepherd-client/src/browser').default;
+const CBrowser = !__DESKTOP__ && !__PLAYER__ && __CLIENT__ && require('shepherd-client/src/browser').default;
 
 
 class RemoteBrowserUI extends Component {