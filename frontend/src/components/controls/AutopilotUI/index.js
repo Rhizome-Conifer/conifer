db74b94b873fc6312c9b7211004001385a7820c1
Mark Beasley <mbeasl1@gmail.com>
2020-04-22 17:09:26 -0400

    Integrate functional behavior api param

--- frontend/src/components/controls/AutopilotUI/index.js
+++ frontend/src/components/controls/AutopilotUI/index.js
@@ -156,14 +156,20 @@ class AutopilotUI extends Component {
                     const name = behavior.get('name');
                     const dt = new Date(behavior.get('updated'));
                     return (
-                      <li onClick={this.selectMode.bind(this, name)} key={behavior.get('name')}>
-                        <input type="radio" name="behavior" value={name} disabled={isRunning || isComplete} aria-labelledby="opt1" onChange={this.handleInput} checked={this.state.behavior === name} />
-                        <div className="desc" id="opt1">
-                          <div className="heading">{behavior.get('displayName') || behavior.get('name')}</div>
+                      <li className={classNames({ disabled: !behavior.get('functional') })} onClick={behavior.get('functional') && this.selectMode.bind(this, name)} key={behavior.get('name')}>
+                        <input type="radio" name="behavior" value={name} disabled={isRunning || isComplete || !behavior.get('functional')} aria-labelledby="opt1" onChange={this.handleInput} checked={this.state.behavior === name} />
+                        <div className="desc">
+                          <h4>{behavior.get('displayName') || behavior.get('name')}</h4>
                           <div className="last-modified">
                             <em>{`Updated: ${dt.toLocaleDateString()} ${dt.toLocaleTimeString()}`}</em>
                           </div>
-                          {behavior.get('description')}
+                          <p>
+                            {behavior.get('description')}
+                          </p>
+                          {
+                            !behavior.get('functional') &&
+                              <div className="note">This behavior is not currently supported. <a href="https://guide.webrecorder.io/#supported-behavior" target="_blank">Learn more</a></div>
+                          }
                         </div>
                       </li>
                     );
