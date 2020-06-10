8b9c222cdcb1617994421efb147c20a5ad42e935
Mark Beasley <mbeasl1@gmail.com>
2020-06-10 10:24:49 -0400

    Copy updates

--- frontend/src/components/siteComponents/HomeUI/index.js
+++ frontend/src/components/siteComponents/HomeUI/index.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
-import { Button } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
 
 import { anonDisabled, homepageAnnouncement, product, supporterPortal } from 'config';
 
@@ -84,17 +84,16 @@ class HomeUI extends PureComponent {
           <div className="intro">
             <h1>{product}</h1>
             <h2>Collect and revisit web pages.</h2>
-            <h4>{product} is a web archiving service to collect and revisit web pages.</h4>
-            <p>{product} creates an interactive copy of any web page that you browse, including content revealed by your interactions such as playing video and audio, scrolling, clicking buttons, and so forth.</p>
+            <p>{product} Conifer is a web archiving service that creates an interactive copy of any web page that you browse, including content revealed by your interactions such as playing video and audio, scrolling, clicking buttons, and so forth.</p>
 
             <div className="cta">
               <Button variant="primary" size="lg" onClick={this.signup}>Creat a Free Account</Button>
               <button className="button-link" onClick={this.login} type="button">Existing Users Login</button>
             </div>
 
-            { supporterPortal && <div className="note">{product} offers free accounts with 5GB of storage. Get more and contribute to Webrecorder's development by <a href={supporterPortal} target="_blank">becoming a supporter</a>.</div> }
+            { supporterPortal && <div className="note">Free accounts with 5GB of storage. Get more and support this project by <a href={supporterPortal} target="_blank">becoming a supporter</a>.</div> }
 
-            <div className="note">Don't want to register? <a href="https://github.com/webrecorder/webrecorder-desktop/releases/latest">Download Desktop App</a> to collect and access archived web pages on your own computer, no account necessary.</div>
+            <div className="note">Conifer is an online service based on <a href="https://webrecorder.net" target="_blank">Webrecorder software</a>.</div>
 
             {
               homepageAnnouncement &&
@@ -152,8 +151,8 @@ class HomeUI extends PureComponent {
           </div>
         </section>
 
-        <section className="advanced-features">
-          <div className="col-xs-10 col-xs-offset-1">
+        <Row as="section" className="advanced-features">
+          <Col xs={{ span: 10, offset: 1}}>
             <h3>Advanced Features</h3>
             <dl>
               <dt>Login and Capture</dt>
@@ -166,54 +165,52 @@ class HomeUI extends PureComponent {
               <dd>Download your web archives in the ISO standard WARC file format.</dd>
 
               <dt>Pre-configured browsers for best capturing results</dt>
-              <dd>Webrecorder's Remote Browser feature provides access to a range of preconfigured browsers running on the Webrecorder server. They offer for the most thorough capture of network traffic, and support for Flash.</dd>
+              <dd>{product}'s Remote Browser feature provides access to a range of preconfigured browsers running on the {product} server. They offer for the most thorough capture of network traffic, and support for Flash.</dd>
             </dl>
-          </div>
-        </section>
+          </Col>
+        </Row>
 
-        <section className="supporter">
-          <header>
-            <h1>Support free, open source tools for archiving the web.</h1>
-          </header>
+        <Row as="section">
+          <Col xs={12} lg={{ span: 10, offset: 1 }} className="supporter">
+            <header>
+              <h1>You can support a free platform for archiving the web.</h1>
+            </header>
 
-          <div className="oss-intro">
-            <h2>{product} is a rapidly developing community project maintained by a non-profit arts organization, Rhizome. Becoming a supporter or donor helps us offset our operational costs, keeping {product} a sustainable project.</h2>
-            <p>In partnership with the Webrecorder Project, we aim to make high-fidelity web archiving accessible to all. Here are some other tools we have developed together. An extensive list of reusable software components produced by {product} and the Webrecorder Project is available here.</p>
-          </div>
+            <div className="oss-intro">
+              <h3>{product} is an open source web archiving initiative by Rhizome, an institution supporting born-digital art and culture</h3>
+              <p>Becoming a supporter or donor helps us offset our operational costs, keeping Conifer a sustainable project.</p>
+            </div>
 
-          {
-            supporterPortal &&
-              <div className="supportCTA">
-                {/*<h3>You can support free, open source tools for archiving the web.</h3>*/}
-                <Button variant="primary" onClick={this.gotToSupporterSite}>Become a Supporter</Button>
-                <a href={supporterPortal} target="_blank">Learn more</a>
-                <p>{product} is a project of Rhizome, a registered 501(c)(3) non-profit organization. Your donations are tax-deductible.</p>
-              </div>
-          }
+            {
+              supporterPortal &&
+                <div className="supportCTA">
+                  {/*<h3>You can support free, open source tools for archiving the web.</h3>*/}
+                  <Button variant="primary" size="lg" onClick={this.gotToSupporterSite}>Become a Supporter</Button>
+                  <a href={supporterPortal} target="_blank">Learn more</a>
+                  <p>{product} is a project of Rhizome, a registered 501(c)(3) non-profit organization. Your donations are tax-deductible.</p>
+                </div>
+            }
 
-          <div className="sub-grid">
-            <div>
+            <div className="tools-intro">
+              <h2>Desktop Tools</h2>
+              <p>In partnership with the Webrecorder project, we aim to make web archiving accessible to all, and ensure interoperability in between tools. Here are some apps we have developed together:</p>
+            </div>
+
+            <div className="tool">
               <img src={require('shared/images/homepage/desktop.png')} alt="Desktop Logo" />
               <h4>Webrecorder Desktop App</h4>
-              <p>Create, manage, and store web archives on your local computer.</p>
+              <p>Create, manage and store web archives on your local computer; import them to Conifer for public presentation.</p>
               <Button variant="primary" onClick={this.desktopApp}>Download Desktop App</Button>
             </div>
 
-            <div>
+            <div className="tool">
               <img src={require('shared/images/homepage/player.png')} alt="Player Logo" />
               <h4>Webrecorder Player App</h4>
-              <p>Use this desktop web archive viewer to browse exported collections, even when you are offline.</p>
+              <p>Export Conifer collections and access them offline.</p>
               <Button variant="primary" onClick={this.playerApp}>Download Player App</Button>
             </div>
-
-            <div>
-              <img src={require('shared/images/homepage/oss.png')} alt="Group of open source software logos" />
-              <h4>Other Software Components</h4>
-              <p>Webrecorder produces a range of re-usable open source software components for web archiving, all of which are available on GitHub.</p>
-              <Button variant="primary" onClick={this.github}>Visit Our Github</Button>
-            </div>
-          </div>
-        </section>
+          </Col>
+        </Row>
       </React.Fragment>
     );
   }
