832956bc4ab51f7c9256f31c27efeb030f25aef0
Mark Beasley <mbeasl1@gmail.com>
2020-11-18 16:49:46 -0500

    Update solr docs ttl

--- webrecorder/webrecorder/solrmanager.py
+++ webrecorder/webrecorder/solrmanager.py
@@ -110,7 +110,7 @@ class SolrManager:
             'timestamp_s': timestamp_s,
             'timestamp_dt': timestamp_dt,
             'mime_s': mime_s,
-            'ttl_s': '+7DAYS'
+            'ttl_s': '+24HOURS'
         }
 
         if text is not None:
